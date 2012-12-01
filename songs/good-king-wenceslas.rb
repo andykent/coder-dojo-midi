cbee578070367e1b66dfaaf96e3194a8b2d286ae
Andy Kent <andy.kent@me.com>
2012-12-01 10:37:16 +0000

    scaffolding for good king impl

--- songs/good-king-wenceslas.rb
+++ songs/good-king-wenceslas.rb
@@ -1,4 +1,13 @@
+require "rubygems"
+require "midi"
+
 # B B B B B B B D G A B
 # C C C C C C B B B B B A A B A ... D!
 # B B B B B B B D G A B
 # C C C C C C B B B B A A B C D ... D!
+
+o = UniMIDI::Output.open(:first)
+
+MIDI.using(o) do
+
+end
