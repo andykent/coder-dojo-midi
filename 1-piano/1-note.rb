bde2e04e7d819dcc3909a417404198f7fae047a0
Andy Kent <andy.kent@me.com>
2012-12-01 10:35:26 +0000

    put note back

--- 1-piano/1-note.rb
+++ 1-piano/1-note.rb
@@ -5,17 +5,6 @@ o = UniMIDI::Output.open(:first)
 
 MIDI.using(o) do
 
-  octave 4
-  channel 1
-
-  []
-
-B B B B B B B D G A B
-C C C C C C B B B B B A A B A ... D!
-B B B B B B B D G A B
-C C C C C C B B B B A A B C D ... D!
-
-
-Good king wenceslas
+  play 'C', 1
 
 end
